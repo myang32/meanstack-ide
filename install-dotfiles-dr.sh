ccc822675a908aca0e488cca7bc86fb2c7f3fe95
StefanScherer <scherer_stefan@icloud.com>
2014-03-18 09:12:50 +0100

    my dotfiles

--- install-dotfiles-dr.sh
+++
@@ -1,8 +0,0 @@
-# ---Install DR .dotfiles ---
-# (run as user)
-
-# clone Dieter's dotfiles
-git clone https://github.com/DieterReuter/dotfiles.git ~/code/dotfiles
-cd ~/code/dotfiles
-./sync.sh -f
-
